1d3a970bd8652331bdb4ab0aeb27513b6b30e0be
liou <liou666@126.com>
2023-02-11 23:51:46 +0800

    fix(ui): name repeat

--- packages/ui/src/components/UserName.vue
+++ packages/ui/src/components/UserName.vue
@@ -7,12 +7,12 @@ const { user } = defineProps<{ user: DY.User }>()
 
 <template>
 	<TBox text-wrap="truncate">
-		<TText wrap="truncate-middle" dimColor v-if="+user.FollowInfo.followerCountStr<20" background-color="#8e44ad">{{' '+ user.nickName.replace(/\r\n/g,"")+' ' }}</TText>
-		<TText wrap="truncate-middle" dimColor v-if="+user.FollowInfo.followerCountStr<100"    background-color="#8e44ad">{{' '+ user.nickName.replace(/\r\n/g,"")+' ' }}</TText>
-		<TText wrap="truncate-middle" dimColor v-else-if="+user.FollowInfo.followerCountStr<500" background-color="blue">{{' '+ user.nickName.replace(/\r\n/g,"")+' ' }}</TText>
-		<TText wrap="truncate-middle" dimColor v-else-if="+user.FollowInfo.followerCountStr<1000" background-color="magenta">{{' '+ user.nickName.replace(/\r\n/g,"")+' '  }}</TText>
-		<TText wrap="truncate-middle" dimColor v-else-if="+user.FollowInfo.followerCountStr<3000" background-color="red">{{ ' '+ user.nickName.replace(/\r\n/g,"")+' ' }}</TText>
-		<TText wrap="truncate-middle" dimColor v-else-if="+user.FollowInfo.followerCountStr<6000" background-color="#34495e">{{ ' '+ user.nickName.replace(/\r\n/g,"")+' ' }}</TText>
-		<TText wrap="truncate-middle" dimColor v-else background-color="#d35400">{{' '+ user.nickName.replace(/\r\n/g,"")+' '  }}</TText>
+		<TText wrap="truncate-middle" dimColor v-if="+user.FollowInfo.followerCountStr>10000" background-color="#d1ccc0">{{' '+ user.nickName.replace(/\r\n/g,"")+' ' }}</TText>
+		<TText wrap="truncate-middle" dimColor v-if="+user.FollowInfo.followerCountStr>6000" background-color="#d35400 ">{{' '+ user.nickName.replace(/\r\n/g,"")+' ' }}</TText>
+		<TText wrap="truncate-middle" dimColor v-else-if="+user.FollowInfo.followerCountStr>3000" background-color="#34495e">{{' '+ user.nickName.replace(/\r\n/g,"")+' ' }}</TText>
+		<TText wrap="truncate-middle" dimColor v-else-if="+user.FollowInfo.followerCountStr>1000" background-color="red">{{' '+ user.nickName.replace(/\r\n/g,"")+' '  }}</TText>
+		<TText wrap="truncate-middle" dimColor v-else-if="+user.FollowInfo.followerCountStr>500" background-color="magenta">{{ ' '+ user.nickName.replace(/\r\n/g,"")+' ' }}</TText>
+		<TText wrap="truncate-middle" dimColor v-else-if="+user.FollowInfo.followerCountStr>200" background-color="blue">{{ ' '+ user.nickName.replace(/\r\n/g,"")+' ' }}</TText>
+		<TText wrap="truncate-middle" dimColor v-else background-color="#8e44ad">{{' '+ user.nickName.replace(/\r\n/g,"")+' '  }}</TText>
 	</TBox>
 </template>
